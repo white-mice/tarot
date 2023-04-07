e9c54eacee4c483a8f007d1e3e9258642efa40c2
Vikram Durai <vikramvdurai@gmail.com>
2023-04-07 17:20:09 +0530

    avatar storage

--- app.mjs
+++ app.mjs
@@ -1,9 +1,9 @@
 import fs from "fs";
 
 let imgs = fs.readdirSync("images");
-let b64Imgs = [];
+let b64Imgs = {};
 imgs.forEach(v => {
-    b64Imgs.push({name: v, data: Buffer.from(fs.readFileSync(`images/${v}`)).toString("base64")});
+    b64Imgs[v] = Buffer.from(fs.readFileSync(`images/${v}`)).toString("base64");
 })
 
 fs.writeFileSync("out.js", `var b64Avatars = ${JSON.stringify(b64Imgs)}`);
